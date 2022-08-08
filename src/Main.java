83cd7b2137657e4b52ed79bfb3e0c722d301c11d
anappgomes <109117667+anappgomes@users.noreply.github.com>
2022-08-08 20:18:17 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente ana = new Cliente();
+		ana.setNome("Ana");
+		
+		Conta cc = new ContaCorrente(ana);
+		Conta poupanca = new ContaPoupanca(ana);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
